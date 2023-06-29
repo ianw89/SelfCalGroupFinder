3d029ae6fefb1baceb71dfaccf6c45f030c875a7
Ian Williams <ianwilliams@nyu.edu>
2023-06-29 17:12:43 -0400

    Conversion script seems to work for no bells and whistles and using the given abs_mag

--- hdf5_to_dat.py
+++ hdf5_to_dat.py
@@ -12,7 +12,7 @@ def abs_mag_r_to_log_solar_L(arr):
 
 # TODO  I'm using 19.5 cut the sample out to 20 above, not 19.5.
 
-def get_max_observable_volume(abs_mags, z_obs m_cut=19.5):
+def get_max_observable_volume(abs_mags, z_obs, m_cut=19.5):
     """
     Calculate the max volume at which the galaxy could be seen in comoving coords.
 
@@ -57,22 +57,50 @@ def main():
 
     if (len(sys.argv) > 1 and sys.argv[1].endswith('.hdf5')):
 
+        outname = sys.argv[1][0:-5] + ".dat"
+        print(outname)
+
         input = h5py.File(sys.argv[1], 'r')
 
         print(list(input['Data']))
 
-        dec = input['Data/dec']
-        ra = input['Data/ra']
-        z = input['Data/z_obs'] 
 
-        abs_mag = input['Data/abs_mag'] 
+        dec = input['Data/dec'][:]
+        ra = input['Data/ra'][:]
+        z = input['Data/z_obs'][:]
+
+        count = len(dec)
+        print(count, "galaxies")
+
+        abs_mag = input['Data/abs_mag'][:]
         log_L_gal = abs_mag_r_to_log_solar_L(abs_mag)
 
-        V_max = 0 # TODO
+        V_max = get_max_observable_volume(abs_mag, z)
+
+
+        colors = np.zeros(count) # TODO compute colors
+        chi = np.zeros(count) # TODO compute chi
+
+        output = np.column_stack((ra, dec, z, log_L_gal, V_max, colors, chi))
+
+        lines = []
+
+        for row in output:
+            lines.append(' '.join(map(str, row)))
+
+        # Oddly doing it this way and not preloading all the ra/decs changes the numbers by a bit
+        # It also takes double as long to do it this way.
+        #for i in range(0, count):
+        #    lines.append(' '.join(map(str, (ra[i], dec[i], z[i], log_L_gal[i], V_max[i], 0, 0 ) )))
+
+        outstr = "\n".join(lines)
+
+        open(outname, 'w').write(outstr)
+
         
 
     else:
-        print("The first argument must be a .hdf5 file path.")
+        print("Usage: hdf5_to_dat.py [input_filename.hdf5]")
 
 
 if __name__ == "__main__":
