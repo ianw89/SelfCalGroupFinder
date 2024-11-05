Code for using corrfunc for projected clustering on catalog. Fix bug in SV3 footprint calculation. Reorganized code a bit.

py/groupcatalog.py
@@ -8,15 +8,12 @@ import pickle
 import subprocess as sp
 from astropy.table import Table
 import astropy.io.fits as fits
-import astropy.constants as const
 import copy
 import sys
-import wp as wp
+import wp
 import math
 from multiprocessing import Pool
-import Corrfunc
-from os.path import dirname, abspath, join as pjoin
-from Corrfunc.io import read_catalog
+from joblib import Parallel, delayed
 
 if './SelfCalGroupFinder/py/' not in sys.path:
     sys.path.append('./SelfCalGroupFinder/py/')
@@ -24,8 +21,6 @@ from pyutils import *
 from redshift_guesser import SimpleRedshiftGuesser, PhotometricRedshiftGuesser
 from hdf5_to_dat import pre_process_mxxl
 from uchuu_to_dat import pre_process_uchuu
-from joblib import Parallel, delayed
-from nnanalysis import cic_binning
 
 # Sentinal value for no truth redshift
 NO_TRUTH_Z = -99.99
@@ -85,6 +80,7 @@ def tile_to_region_raw(key):
     return sv3_tile_to_region.get(key, None)  # Return None if key is not found
 tile_to_region = np.vectorize(tile_to_region_raw)
 
+CLUSTERING_MAG_BINS = [-16, -17, -18, -19, -20, -21, -22, -23]
 
 GF_PROPS_VANILLA = {
     'zmin':0, 
@@ -135,7 +131,10 @@ class GroupCatalog:
         self.L_gal_bins = L_gal_bins
         self.L_gal_labels = L_gal_labels
 
-        self.wp_all = None
+        self.wp_all = None # (rbins, wp_all, wp_r, wp_b)
+        self.wp_all_extra = None # (rbins, wp_all, wp_r, wp_b)
+        self.wp_slices = np.array(len(CLUSTERING_MAG_BINS) * [None]) # Tuple of (rbins, wp) at each index
+        self.wp_slices_extra = np.array(len(CLUSTERING_MAG_BINS) * [None]) # Tuple of (rbins, wp) at each index
 
         # Geneated from popmock option in group finder
         self.mock_b_M17 = None
@@ -622,6 +621,8 @@ class BGSGroupCatalog(GroupCatalog):
             infile = IAN_BGS_MERGED_FILE_OLD
         elif self.data_cut == "Y3-Kibo":
             infile = IAN_BGS_Y3_MERGED_FILE
+        elif self.data_cut == "Y3-Kibo-SV3Cut":
+            infile = IAN_BGS_Y3_MERGED_FILE
         elif self.data_cut == "Y3-Jura":
             infile = IAN_BGS_Y3_MERGED_FILE_JURA
         elif self.data_cut == "sv3":
@@ -719,121 +720,172 @@ class BGSGroupCatalog(GroupCatalog):
         if self.data_cut == 'sv3':
             self.centered = filter_SV3_to_avoid_edges(self)
 
+        if self.data_cut == 'sv3' or self.data_cut == 'Y3-Kibo-SV3Cut':
+            self.calculate_projected_clustering()
+            self.calculate_projected_clustering_in_magbins()
+
         print("Post-processing done.")
 
-    def calculate_projected_clustering(self):
-        """
-        Create subsamples of this BGS catalog and calculate projected clustering for each.
-        """
 
-        if self.data_cut != 'sv3':
-            raise ValueError("calculate_projected_clustering() only works for SV3 right now")
+    def calculate_projected_clustering(self, with_extra_randoms=False):
         print("Calculating projected clustering...")
-        # See how C code does volume limited sample
 
         # TODO weights for fiber collisions corrections
 
-        # For each abs mag bin, calculate projected clustering
+        df = self.all_data
+        if df.get('mag_R') is None:
+            df['mag_R'] = log_solar_L_to_abs_mag_r(np.log10(df['L_gal']))
+            df['mag_bin'] = np.digitize(df.mag_R, CLUSTERING_MAG_BINS)
 
-        """
-        TODO these should have zmin as well, to be like SDSS versions.
-        Mag-5log(h) > -14:  zmax theory=0.01650  zmax obs=0.015029
-        Mag-5log(h) > -15:  zmax theory=0.02595  zmax obs=0.027139
-        Mag-5log(h) > -16:  zmax theory=0.04067  zmax obs=0.043211
-        Mag-5log(h) > -17:  zmax theory=0.06336  zmax obs=0.06597
-        Mag-5log(h) > -18:  zmax theory=0.09792  zmax obs=0.101448
-        Mag-5log(h) > -19:  zmax theory=0.14977  zmax obs=0.154458
-        Mag-5log(h) > -20:  zmax theory=0.22620  zmax obs=0.231856
-        Mag-5log(h) > -21:  zmax theory=0.33694  zmax obs=0.331995
-        Mag-5log(h) > -22:  zmax theory=0.49523  zmax obs=0.459593
-        Mag-5log(h) > -23:  zmax theory=0.72003  zmax obs=0.49971
-        """
-        mag_bin = [-16, -17, -18, -19, -20, -21, -22, -23]
-        zmax = [0.04067, 0.06336, 0.09792, 0.14977, 0.22620, 0.33694, 0.49523, 0.72003]
+        if with_extra_randoms:
+            randoms = pickle.load(open(MY_RANDOMS_SV3, "rb"))
+            print(f"Random count / data count = {len(randoms['RA'])} / {len(df)}")
+            self.wp_all_extra = wp.calculate_wp_from_df(df, randoms)
+
+        randoms = pickle.load(open(MY_RANDOMS_SV3_MINI, "rb"))
+        print(f"Random count / data count = {len(randoms['RA'])} / {len(df)}")
+        self.wp_all = wp.calculate_wp_from_df(df, randoms)
+
+    
+
+    def calculate_projected_clustering_in_magbins(self, with_extra_randoms=False):
+        print("Calculating luminosity dependent clustering...")
+
+        #Mag-5log(h) > -14:  zmax theory=0.01650  zmax obs=0.015029
+        #Mag-5log(h) > -15:  zmax theory=0.02595  zmax obs=0.027139
+        #Mag-5log(h) > -16:  zmax theory=0.04067  zmax obs=0.043211
+        #Mag-5log(h) > -17:  zmax theory=0.06336  zmax obs=0.06597
+        #Mag-5log(h) > -18:  zmax theory=0.09792  zmax obs=0.101448
+        #Mag-5log(h) > -19:  zmax theory=0.14977  zmax obs=0.154458
+        #Mag-5log(h) > -20:  zmax theory=0.22620  zmax obs=0.231856
+        #Mag-5log(h) > -21:  zmax theory=0.33694  zmax obs=0.331995
+        #Mag-5log(h) > -22:  zmax theory=0.49523  zmax obs=0.459593
+        #Mag-5log(h) > -23:  zmax theory=0.72003  zmax obs=0.49971
+        zmax = [0.04067, 0.06336, 0.09792, 0.14977, 0.22620, 0.331995, 0.459593, 0.49971]
+        #Not doing zmin's for now; no need for indepdenent samples for this
 
         df = self.all_data
-        df['mag_R'] = log_solar_L_to_abs_mag_r(np.log10(df['L_gal']))
-        df['mag_bin'] = np.digitize(df.mag_R, mag_bin)
-        cz = df.z.to_numpy().copy() * const.c.to('km/s').value
+        if df.get('mag_R') is None:
+            df['mag_R'] = log_solar_L_to_abs_mag_r(np.log10(df['L_gal']))
+            df['mag_bin'] = np.digitize(df.mag_R, CLUSTERING_MAG_BINS)
 
-        randoms = pickle.load(open(MY_RANDOMS_SV3, "rb"))
-        # Pull random values from df.z since the randoms don't contain z
-        rand_cz = np.random.choice(df.loc[z_flag_is_spectro_z(df.z_assigned_flag), 'z'], size=len(randoms['RA']), replace=True) * const.c.to('km/s').value
-    
-        # Overall sample
-        rbins, wp_all = calculate_wp(
-            df.RA.to_numpy(), 
-            df.Dec.to_numpy(), 
-            cz, 
-            randoms['RA'],
-            randoms['Dec'], 
-            rand_cz)
-        rbins, wp_red = calculate_wp(
-            df.loc[df.quiescent].RA.to_numpy(),
-            df.loc[df.quiescent].Dec.to_numpy(),
-            cz[df.quiescent],
-            randoms['RA'],
-            randoms['Dec'],
-            rand_cz)
-        rbins, wp_blue = calculate_wp(
-            df.loc[~df.quiescent].RA.to_numpy(),
-            df.loc[~df.quiescent].Dec.to_numpy(),
-            cz[~df.quiescent],
-            randoms['RA'],
-            randoms['Dec'],
-            rand_cz)
+        if with_extra_randoms:
+            randoms = pickle.load(open(MY_RANDOMS_SV3, "rb"))
+            for i in range(len(CLUSTERING_MAG_BINS)):
+                in_z_range = df.z < zmax[i]
+                in_mag_bin = df.mag_bin == i
+                mag_min = CLUSTERING_MAG_BINS[i-1] if i > 0 else CLUSTERING_MAG_BINS[i]
+                mag_max = CLUSTERING_MAG_BINS[i]
+                rows = in_z_range & in_mag_bin
 
-        self.wp_all = (rbins, wp_all, wp_red, wp_blue)
-        self.wp_slices = np.array(len(mag_bin) * [None]) # Tuple of (rbins, wp) at each index
+                rbins, wp_a, wp_r, wp_b = wp.calculate_wp_from_df(df.loc[rows], randoms)
 
-        # In mag bins (and red blue split), calculate wp
-        for i in range(len(mag_bin)):
+                self.wp_slices_extra[i] = (rbins, wp_a, wp_r, wp_b, mag_min, mag_max)
+
+        randoms = pickle.load(open(MY_RANDOMS_SV3_MINI, "rb"))
+
+        for i in range(len(CLUSTERING_MAG_BINS)):
             in_z_range = df.z < zmax[i]
             in_mag_bin = df.mag_bin == i
-            mag_min = mag_bin[i-1] if i > 0 else mag_bin[i]
-            mag_max = mag_bin[i]
-            red = df.quiescent
+            mag_min = CLUSTERING_MAG_BINS[i-1] if i > 0 else CLUSTERING_MAG_BINS[i]
+            mag_max = CLUSTERING_MAG_BINS[i]
             rows = in_z_range & in_mag_bin
 
-            rbins, wp_all = calculate_wp(
-                df.loc[rows].RA.to_numpy(), 
-                df.loc[rows].Dec.to_numpy(), 
-                cz[rows], 
-                randoms['RA'],
-                randoms['Dec'], 
-                rand_cz)
+            rbins, wp_a, wp_r, wp_b = wp.calculate_wp_from_df(df.loc[rows], randoms)
 
-            rows = in_z_range & in_mag_bin & red
-            print("RED")
-            if i == 0:
-                print(f"zmax={zmax[i]:.5f}, {mag_bin[i]} < mag, n={len(df.loc[rows])}")
-            else:
-                print(f"zmax={zmax[i]:.5f}, {mag_bin[i-1]} < mag < {mag_bin[i]}, n={len(df.loc[rows])}")
-
-            rbins, wp_red = calculate_wp(
-                df.loc[rows].RA.to_numpy(), 
-                df.loc[rows].Dec.to_numpy(), 
-                cz[rows], 
-                randoms['RA'],
-                randoms['Dec'], 
-                rand_cz)
+            self.wp_slices[i] = (rbins, wp_a, wp_r, wp_b, mag_min, mag_max)
             
-            rows = in_z_range & in_mag_bin & ~red
-            print("BLUE")
-            if i == 0:
-                print(f"zmax={zmax[i]:.5f}, {mag_bin[i]} < mag, n={len(df.loc[rows])}")
+    
+    def add_jackknife_err_to_proj_clustering(self, with_extra_randoms=False, for_mag_bins=False):
+
+        if self.data_cut != 'sv3' and self.data_cut != 'Y3-Kibo-SV3Cut':
+            print("Warning: add_jackknife_err_to_proj_clustering called for non-SV3 data cut. Skipping.")
+            return
+        
+        t1 = time.time()
+        print("Adding jackknife error to projected clustering...")
+        
+        df = self.all_data
+        if with_extra_randoms:
+            randoms = pickle.load(open(MY_RANDOMS_SV3, "rb"))
+        else:
+            randoms = pickle.load(open(MY_RANDOMS_SV3_MINI, "rb"))
+
+        # label the SV3 region each galaxy is in
+        df['region'] = tile_to_region(df['nearest_tile_id']).astype(int)
+        region_ids = df['region'].unique()
+        n = len(region_ids)
+
+        def jacknife_iteration(region_idx_to_drop, mag_bin=None):
+            if mag_bin is not None:
+                in_mag_bin = df.mag_bin == mag_bin
+                in_z_range = df.z < zmax[mag_bin]
+                alt_df = df.loc[np.all([(df['region'] != region_idx_to_drop), in_mag_bin, in_z_range], axis=0)]
             else:
-                print(f"zmax={zmax[i]:.5f}, {mag_bin[i-1]} < mag < {mag_bin[i]}, n={len(df.loc[rows])}")
+                alt_df = df.loc[df['region'] != region_idx_to_drop]
+            print(f"Jackknife cut out region {region_idx_to_drop} with {len(df)-len(alt_df)} galaxies.")
+            return wp.calculate_wp_from_df(alt_df, randoms)
 
-            rbins, wp_blue = calculate_wp(
-                df.loc[rows].RA.to_numpy(), 
-                df.loc[rows].Dec.to_numpy(), 
-                cz[rows],
-                randoms['RA'],
-                randoms['Dec'], 
-                rand_cz)
+        def cov(realizations):
+            mean = np.mean(realizations, axis=0)
+            cov = np.zeros((mean.size, mean.size))
+            for i in range(n):
+                diff = realizations[i] - mean
+                cov += np.outer(diff, diff)
+            cov *= (n - 1) / n
+            return cov
 
-            self.wp_slices[i] = (rbins, wp_all, wp_red, wp_blue, mag_min, mag_max)
+        if for_mag_bins:
+            zmax = [0.04067, 0.06336, 0.09792, 0.14977, 0.22620, 0.331995, 0.459593, 0.49971]
+            self.wp_slices_cov = np.array(len(CLUSTERING_MAG_BINS) * [None])
+            self.wp_slices_r_cov = np.array(len(CLUSTERING_MAG_BINS) * [None])
+            self.wp_slices_b_cov = np.array(len(CLUSTERING_MAG_BINS) * [None])
+            self.wp_slices_err = np.array(len(CLUSTERING_MAG_BINS) * [None])
+            self.wp_slices_r_err = np.array(len(CLUSTERING_MAG_BINS) * [None])
+            self.wp_slices_b_err = np.array(len(CLUSTERING_MAG_BINS) * [None])
+
+            for i in range(len(CLUSTERING_MAG_BINS)):
+                wp_realizations = np.zeros((len(region_ids), wp.NBINS), dtype=float)
+                wp_r_realizations = np.zeros((len(region_ids), wp.NBINS), dtype=float)
+                wp_b_realizations = np.zeros((len(region_ids), wp.NBINS), dtype=float)
+
+                j = 0
+                for rid in region_ids:
+                    rbins, wp_a, wp_r, wp_b = jacknife_iteration(rid, mag_bin=i)
+                    wp_realizations[j, :] = wp_a
+                    wp_r_realizations[j, :] = wp_r
+                    wp_b_realizations[j, :] = wp_b
+                    j += 1
+
+                self.wp_slices_cov[i] = cov(wp_realizations)
+                self.wp_slices_r_cov[i] = cov(wp_r_realizations)
+                self.wp_slices_b_cov[i] = cov(wp_b_realizations)
+                self.wp_slices_err[i] = np.sqrt(np.diag(self.wp_slices_cov[i]))
+                self.wp_slices_r_err[i] = np.sqrt(np.diag(self.wp_slices_r_cov[i]))
+                self.wp_slices_b_err[i] = np.sqrt(np.diag(self.wp_slices_b_cov[i]))
+
+        else:
+            wp_realizations = np.zeros((len(region_ids), wp.NBINS), dtype=float)
+            wp_r_realizations = np.zeros((len(region_ids), wp.NBINS), dtype=float)
+            wp_b_realizations = np.zeros((len(region_ids), wp.NBINS), dtype=float)
+
+            i = 0
+            for rid in region_ids:
+                rbins, wp_a, wp_r, wp_b = jacknife_iteration(rid)
+                wp_realizations[i, :] = wp_a
+                wp_r_realizations[i, :] = wp_r
+                wp_b_realizations[i, :] = wp_b
+                i += 1
+
+            self.wp_cov = cov(wp_realizations)
+            self.wp_r_cov = cov(wp_r_realizations)
+            self.wp_b_cov = cov(wp_b_realizations)
+            self.wp_err = np.sqrt(np.diag(self.wp_cov))
+            self.wp_r_err = np.sqrt(np.diag(self.wp_r_cov))
+            self.wp_b_err = np.sqrt(np.diag(self.wp_b_cov))
+
+        t2 = time.time()
+        print(f"Jackknife error of wp complete in {t2-t1:.2} seconds.")
 
 
     def refresh_df_views(self):
@@ -1023,8 +1075,14 @@ def get_footprint_fraction(data_cut, mode, num_passes_required):
         FOOTPRINT_FRAC_3pass = 0.2338243 # 9645 degrees
         FOOTPRINT_FRAC_4pass = 0.1150345 # 4745 degrees
     elif data_cut == "sv3":
-        FOOTPRINT_FRAC_1pass = 173.87 / DEGREES_ON_SPHERE 
-        FOOTPRINT_FRAC_10pass = 138.192 / DEGREES_ON_SPHERE 
+        # These are for the 18/20 patches being used. We dropped two due to poor Y3 overlap.
+        FOOTPRINT_FRAC_1pass =  156.2628 / DEGREES_ON_SPHERE 
+        FOOTPRINT_FRAC_10pass = 124.2812 / DEGREES_ON_SPHERE 
+    elif data_cut == "Y3-Kibo-SV3Cut":
+        # Here the data was cut to the SV3 10p footprint. 
+        # But the num_passes is now referring to actualy Y3 main survey passes.
+        FOOTPRINT_FRAC_1pass = FOOTPRINT_FRAC_10pass # See the confusion?
+        # Not computed for other situations.
     else:
         print("Invalid data cut. Exiting.")
         exit(2)
@@ -1164,7 +1222,9 @@ def pre_process_BGS(fname, mode, outname_base, APP_MAG_CUT, CATALOG_APP_MAG_CUT,
     # NTILE_MINE gives how many tiles include just from inclusion in circles drawn around tile centers
     # null values (masked rows) are unobserved targets; not all columns are masked though
 
+    ##############################################################################
     # Make filter arrays (True/False values)
+    ##############################################################################
     multi_pass_filter = table['NTILE_MINE'] >= num_passes_required
     galaxy_observed_filter = obj_type == b'GALAXY'
     app_mag_filter = app_mag_r < APP_MAG_CUT
@@ -1172,9 +1232,14 @@ def pre_process_BGS(fname, mode, outname_base, APP_MAG_CUT, CATALOG_APP_MAG_CUT,
     redshift_hi_filter = z_obs < Z_MAX
     deltachi2_filter = deltachi2 > 40 # Ensures that there wasn't another z with similar likelihood from the z fitting code
     
-    ##############################################################################
+    # Special version cut to look like SV3 - choose only the ones inside the SV3 footprint
+    if data_cut == "Y3-Kibo-SV3Cut":
+        region = tile_to_region(ntid)
+        to_remove = np.isin(region, sv3_poor_y3overlap)
+        in_good_sv3regions = ~to_remove
+        multi_pass_filter = np.all([multi_pass_filter, table['NTILE_MINE_SV3'] >= 10, in_good_sv3regions], axis=0)
+
     # Roughly remove HII regions of low z, high angular size galaxies (SGA catalog)
-    ##############################################################################
     if maskbits is not None and ref_cat is not None:
         BITMASK_SGA = 0x1000 
         sga_collision = (maskbits & BITMASK_SGA) != 0

py/plotting.py
@@ -336,6 +336,8 @@ def plots(*catalogs, show_err=None, truth_on=False):
     #ax2.set_ylabel('$N_{sat}$')
     fig.tight_layout()
 
+    qf_cen_plot(*catalogs)
+
     if len(catalogs) == 1:
         plots_color_split(*catalogs, total_on=True)
 
@@ -346,11 +348,18 @@ def plots(*catalogs, show_err=None, truth_on=False):
 
 def wp_rp(catalog: GroupCatalog):
     colors = ['k', 'r', 'b']
-    plt.figure(figsize=(6, 6))
-    plt.plot(catalog.wp_all[0][:-1], catalog.wp_all[1], marker='o', linestyle='-', label='All', color=colors[0])
-    plt.plot(catalog.wp_all[0][:-1], catalog.wp_all[2], marker='o', linestyle='-', label='Red', color=colors[1])
-    plt.plot(catalog.wp_all[0][:-1], catalog.wp_all[3], marker='o', linestyle='-', label='Blue', color=colors[2])
+    f = catalog
+    plt.figure(figsize=(5, 5))
+    if hasattr(f, 'wp_err'):
+        plt.errorbar(f.wp_all[0][:-1], f.wp_all[1], yerr=f.wp_err, marker='o', linestyle='-', label='All', color=colors[0])
+        plt.errorbar(f.wp_all[0][:-1], f.wp_all[2], yerr=f.wp_r_err, marker='o', linestyle='-', label='Red', color=colors[1])
+        plt.errorbar(f.wp_all[0][:-1], f.wp_all[3], yerr=f.wp_b_err, marker='o', linestyle='-', label='Blue', color=colors[2])
+    else:
+        plt.plot(catalog.wp_all[0][:-1], catalog.wp_all[1], marker='o', linestyle='-', label='All', color=colors[0])
+        plt.plot(catalog.wp_all[0][:-1], catalog.wp_all[2], marker='o', linestyle='-', label='Red', color=colors[1])
+        plt.plot(catalog.wp_all[0][:-1], catalog.wp_all[3], marker='o', linestyle='-', label='Blue', color=colors[2])
     plt.xscale('log')
+    plt.ylim(8, 2000)
     plt.yscale('log')
     plt.xlabel(r'$r_p$ [Mpc/h]')
     plt.ylabel(r'$w_p(r_p)$')
@@ -359,124 +368,84 @@ def wp_rp(catalog: GroupCatalog):
     plt.grid(True)
     plt.show()
 
+def wp_rp_magbins(catalog: GroupCatalog):
+    colors = ['k', 'r', 'b']
+    f = catalog
+
     # Additional rows for each magnitude slice in wp_slices
-    fig, axes = plt.subplots(len(catalog.wp_slices) - 2, 3, figsize=(18, 6 * (len(catalog.wp_slices) - 2)))
+    fig, axes = plt.subplots(2, 3, figsize=(12, 9))
 
     for i, mag_slice in enumerate(catalog.wp_slices):
-        if i == 0:
+        if i <= 1:
             continue
         if i == len(catalog.wp_slices) - 1:
-            mag_range_label = f"{catalog.wp_slices[i][4]:.1f} > $M_r$-5log(h)"
+            mag_range_label = f"{catalog.wp_slices[i][4]:.1f} > $M_r$ - 5log($h$)"
         else:
-            mag_range_label = f"{catalog.wp_slices[i][4]:.1f} > $M_r$-5log(h) > {catalog.wp_slices[i][5]:.1f}"
+            mag_range_label = f"{catalog.wp_slices[i][4]:.1f} > $M_r$ - 5log($h$) > {catalog.wp_slices[i][5]:.1f}"
 
-        row = i - 1
+        row = (i-2) // 3
+        col = (i-2) % 3
 
-        for f in [catalog]:
-            axes[row, 0].plot(f.wp_slices[i][0][:-1], f.wp_slices[i][1], marker='o', linestyle='-', label=f.name, color=colors[0])
-        axes[row, 0].set_xscale('log')
-        axes[row, 0].set_yscale('log')
-        axes[row, 0].set_xlabel(r'$r_p$ [Mpc/h]')
-        axes[row, 0].set_ylabel(r'$w_p(r_p)$')
-        axes[row, 0].set_title(f'Overall $w_p(r_p)$ - {mag_range_label}')
-        axes[row, 0].grid(True)
-        axes[row, 0].legend()
+        axes[row, col].plot(f.wp_slices[i][0][:-1], f.wp_slices[i][1], marker='o', linestyle='-', label=f.name, color=colors[0])
+        axes[row, col].plot(f.wp_slices[i][0][:-1], f.wp_slices[i][2], marker='o', linestyle='-', label=f.name, color=colors[1])
+        axes[row, col].plot(f.wp_slices[i][0][:-1], f.wp_slices[i][3], marker='o', linestyle='-', label=f.name, color=colors[2])
+        axes[row, col].set_xscale('log')
+        axes[row, col].set_ylim(8, 2000)
+        axes[row, col].set_yscale('log')
+        axes[row, col].set_xlabel(r'$r_p$ [Mpc/h]')
+        axes[row, col].set_ylabel(r'$w_p(r_p)$')
+        axes[row, col].set_title(f'{mag_range_label}')
+        axes[row, col].grid(True)
 
-        for f in [catalog]:
-            axes[row, 1].plot(f.wp_slices[i][0][:-1], f.wp_slices[i][2], marker='o', linestyle='-', label=f.name, color=colors[1])
-        axes[row, 1].set_xscale('log')
-        axes[row, 1].set_yscale('log')
-        axes[row, 1].set_xlabel(r'$r_p$ [Mpc/h]')
-        axes[row, 1].set_ylabel(r'$w_p(r_p)$')
-        axes[row, 1].set_title(f'Red $w_p(r_p)$ - {mag_range_label}')
-        axes[row, 1].grid(True)
-
-        for f in [catalog]:
-            axes[row, 2].plot(f.wp_slices[i][0][:-1], f.wp_slices[i][3], marker='o', linestyle='-', label=f.name, color=colors[2])
-        axes[row, 2].set_xscale('log')
-        axes[row, 2].set_yscale('log')
-        axes[row, 2].set_xlabel(r'$r_p$ [Mpc/h]')
-        axes[row, 2].set_ylabel(r'$w_p(r_p)$')
-        axes[row, 2].set_title(f'Blue $w_p(r_p)$ - {mag_range_label}')
-        axes[row, 2].grid(True)
 
     plt.tight_layout()
     plt.show()
 
 
+def compare_wp_rp(d1, d2_t):
+    def plot_fractional_difference(ax, wp1, wp2, wp1_red, wp2_red, wp1_blue, wp2_blue, label):
+        percent_diff = 100 * (wp1 - wp2) / wp2
+        percent_diff_r = 100 * (wp1_red - wp2_red) / wp2_red
+        percent_diff_b = 100 * (wp1_blue - wp2_blue) / wp2_blue
 
-def compare_wp_rp(*datasets):
-    fig, axes = plt.subplots(1, 3, figsize=(18, 6))
+        ax.plot(d1.wp_all[0][:-1], percent_diff, marker='o', linestyle='-', color='black', label=f'Overall {label}')
+        ax.plot(d1.wp_all[0][:-1], percent_diff_r, marker='o', linestyle='-', color='red', label=f'Red {label}')
+        ax.plot(d1.wp_all[0][:-1], percent_diff_b, marker='o', linestyle='-', color='blue', label=f'Blue {label}')
+        
+        ax.set_xscale('log')
+        ax.set_ylim(-15, 15)
+        ax.set_yscale('linear')
+        ax.set_xlabel(r'$r_p$ [Mpc/h]')
+        ax.set_ylabel(r'$w_p(r_p)$ Difference (%)')
+        ax.grid(True)
 
-    for f in datasets:
-        axes[0].plot(f.wp_all[0][:-1], f.wp_all[1], marker='o', linestyle='-', label=f.name, color=f.color)
-    axes[0].set_xscale('log')
-    axes[0].set_yscale('log')
-    axes[0].set_xlabel(r'$r_p$ [Mpc/h]')
-    axes[0].set_ylabel(r'$w_p(r_p)$')
-    axes[0].set_title('Projected Correlation Function $w_p(r_p)$')
-    axes[0].grid(True)
-    axes[0].legend()
-
-    for f in datasets:
-        axes[1].plot(f.wp_all[0][:-1], f.wp_all[2], marker='o', linestyle='-', label=f.name, color=f.color)
-    axes[1].set_xscale('log')
-    axes[1].set_yscale('log')
-    axes[1].set_xlabel(r'$r_p$ [Mpc/h]')
-    axes[1].set_ylabel(r'$w_p(r_p)$')
-    axes[1].set_title('Projected Correlation Function Red $w_p(r_p)$')
-    axes[1].grid(True)
-
-    for f in datasets:
-        axes[2].plot(f.wp_all[0][:-1], f.wp_all[3], marker='o', linestyle='-', label=f.name, color=f.color)
-    axes[2].set_xscale('log')
-    axes[2].set_yscale('log')
-    axes[2].set_xlabel(r'$r_p$ [Mpc/h]')
-    axes[2].set_ylabel(r'$w_p(r_p)$')
-    axes[2].set_title('Projected Correlation Function Blue $w_p(r_p)$')
-    axes[2].grid(True)
+    fig, ax = plt.subplots(figsize=(5, 5))
+    plot_fractional_difference(ax, d1.wp_all[1], d2_t.wp_all[1], d1.wp_all[2], d2_t.wp_all[2], d1.wp_all[3], d2_t.wp_all[3], 'Flux-limited')
+    
+    # Show a shaded region for the error in the d2 set. 
+    ax.fill_between(d2_t.wp_all[0][:-1], 100*d2_t.wp_err/d2_t.wp_all[1], -100*d2_t.wp_err/d2_t.wp_all[1], color='black', alpha=0.2)
+    ax.fill_between(d2_t.wp_all[0][:-1], 100*d2_t.wp_r_err/d2_t.wp_all[2], -100*d2_t.wp_r_err/d2_t.wp_all[2], color='red', alpha=0.25)
+    ax.fill_between(d2_t.wp_all[0][:-1], 100*d2_t.wp_b_err/d2_t.wp_all[3], -100*d2_t.wp_b_err/d2_t.wp_all[3], color='blue', alpha=0.25)
+    
+    ax.set_title('$w_p(r_p)$ Fractional Difference')
+    plt.legend()
 
     plt.tight_layout()
     plt.show()
 
     # Additional rows for each magnitude slice in wp_slices
-    fig, axes = plt.subplots(len(datasets[0].wp_slices) - 2, 3, figsize=(18, 6 * (len(datasets[0].wp_slices) - 2)))
+    fig, axes = plt.subplots(2, 3, figsize=(12, 9))
 
-    for i, mag_slice in enumerate(datasets[0].wp_slices):
-        if i == 0:
+    for i, mag_slice in enumerate(d1.wp_slices):
+        if i <= 1:
             continue
 
-        mag_range_label = f"{datasets[0].wp_slices[i][4]:.1f} > $M_r$-5log(h) > {datasets[0].wp_slices[i][5]:.1f}"
+        mag_range_label = f"{d1.wp_slices[i][4]:.1f} > $M_r$ - 5log($h$) > {d1.wp_slices[i][5]:.1f}"
+        row = (i - 2) // 3
+        col = (i - 2) % 3
 
-        row = i - 1
-
-        for f in datasets:
-            axes[row, 0].plot(f.wp_slices[i][0][:-1], f.wp_slices[i][1], marker='o', linestyle='-', label=f.name, color=f.color)
-        axes[row, 0].set_xscale('log')
-        axes[row, 0].set_yscale('log')
-        axes[row, 0].set_xlabel(r'$r_p$ [Mpc/h]')
-        axes[row, 0].set_ylabel(r'$w_p(r_p)$')
-        axes[row, 0].set_title(f'Overall $w_p(r_p)$\n{mag_range_label}')
-        axes[row, 0].grid(True)
-        axes[row, 0].legend()
-
-        for f in datasets:
-            axes[row, 1].plot(f.wp_slices[i][0][:-1], f.wp_slices[i][2], marker='o', linestyle='-', label=f.name, color=f.color)
-        axes[row, 1].set_xscale('log')
-        axes[row, 1].set_yscale('log')
-        axes[row, 1].set_xlabel(r'$r_p$ [Mpc/h]')
-        axes[row, 1].set_ylabel(r'$w_p(r_p)$')
-        axes[row, 1].set_title(f'Red $w_p(r_p)$\n{mag_range_label}')
-        axes[row, 1].grid(True)
-
-        for f in datasets:
-            axes[row, 2].plot(f.wp_slices[i][0][:-1], f.wp_slices[i][3], marker='o', linestyle='-', label=f.name, color=f.color)
-        axes[row, 2].set_xscale('log')
-        axes[row, 2].set_yscale('log')
-        axes[row, 2].set_xlabel(r'$r_p$ [Mpc/h]')
-        axes[row, 2].set_ylabel(r'$w_p(r_p)$')
-        axes[row, 2].set_title(f'Blue $w_p(r_p)$\n{mag_range_label}')
-        axes[row, 2].grid(True)
+        plot_fractional_difference(axes[row, col], d1.wp_slices[i][1], d2_t.wp_slices[i][1], d1.wp_slices[i][2], d2_t.wp_slices[i][2], d1.wp_slices[i][3], d2_t.wp_slices[i][3], f'\n{mag_range_label}')
+        axes[row, col].set_title(f'{mag_range_label}')
 
     plt.tight_layout()
     plt.show()
@@ -1340,6 +1309,115 @@ def assigned_halo_analysis(*sets):
         #plt.xlabel('$z_{eff}$ (effective/assigned redshift)')
         #plt.ylabel('Fraction Assigned Halo = True Host Halo')
         
+
+
+
+
+
+###############################################
+# Routines for making RA DEC maps
+###############################################
+
+# 36*18*640 / 41253 = 10.05 dots per sq degree
+# so a dot size of 1 is 0.1 sq deg
+
+# 0.00191 sq deg is area of a fiber's region
+# that is the size we want to draw for each galaxy
+#so s=0.01 is what we want
+
+#def molleweide_map_for_catalog(catalog: GroupCatalog, alpha=0.1, dpi=150, fig=None, dotsize=0.01):
+#    return make_map(catalog.all_data.RA.to_numpy(), catalog.all_data.Dec.to_numpy(), alpha, dpi, fig, dotsize)
+
+def make_map(ra, dec, alpha=0.1, dpi=150, fig=None, dotsize=0.01):
+    """
+    Give numpy array of ra and dec.
+    """
+
+    if np.any(ra > 180.0): # if data given is 0 to 360
+        assert np.all(ra > -0.1)
+        ra = ra - 180
+    if np.any(dec > 90.0): # if data is 0 to 180
+        print(f"WARNING: Dec values are 0 to 180. Subtracting 90 to get -90 to 90.")
+        assert np.all(dec > -0.1)
+        dec = dec - 90
+
+    # Build a map of the galaxies
+    ra_angles = coord.Angle(ra*u.degree)
+    ra_angles = ra_angles.wrap_at(180*u.degree)
+    #ra_angles = ra_angles.wrap_at(360*u.degree)
+    dec_angles = coord.Angle(dec*u.degree)
+
+    if fig == None:
+        fig = plt.figure(figsize=(12,12))
+        fig.dpi=dpi
+        ax = fig.add_subplot(111, projection="mollweide")
+        plt.grid(visible=True, which='both')
+        ax.set_xticklabels(['30°', '60°', '90°', '120°', '150°', '180°', '210°', '240°', '270°', '300°', '330°'])
+    else:
+        ax=fig.get_axes()[0]
+
+    ax.scatter(ra_angles.radian, dec_angles.radian, alpha=alpha, s=dotsize)
+    return fig
+
+
+
+def plot_positions(*dataframes, tiles_df: pd.DataFrame = None, DEG_LONG=1, split=True, ra_min=30, dec_min = -5):
+
+    ra_max = ra_min + DEG_LONG
+    dec_max = dec_min + DEG_LONG
+
+    fig,ax = plt.subplots(1)
+    fig.set_size_inches(10*DEG_LONG + 1,10*DEG_LONG + 1) # the extra inch is because of the frame, rough correction
+    dpi = 100
+    fig.set_dpi(dpi)
+    dots_per_sqdeg = 10 * 10 * dpi # so 10,000 dots in a square degree
+    ax.set_aspect('equal')
+    ax.set_xlabel("RA [deg]")
+    ax.set_ylabel("Dec [deg]")
+    ax.set_xlim(ra_min, ra_max)
+    ax.set_ylim(dec_min, dec_max)
+
+    if tiles_df is not None:
+        TILE_RADIUS = 5862.0 * u.arcsec # arcsec
+        tile_radius = TILE_RADIUS.to(u.degree).value
+        circle_ra_max = ra_max + 2*tile_radius
+        circle_ra_min = ra_min - 2*tile_radius
+        circle_dec_max = dec_max + 2*tile_radius
+        circle_dec_min = dec_min - 2*tile_radius
+        tiles_to_draw = tiles_df.query('RA < @circle_ra_max and RA > @circle_ra_min and Dec < @circle_dec_max and Dec > @circle_dec_min')
+        
+        for index, row in tiles_df.iterrows():
+            circ = Circle((row.RA, row.Dec), tile_radius, color='k', fill=False, lw=10)
+            ax.add_patch(circ)
+
+    alpha = 1 if len(dataframes) == 1 else 0.5
+    for d in dataframes:
+        plot_ra_dec_inner(d, ax, dots_per_sqdeg, ra_min, ra_max, dec_min, dec_max, split, alpha)
+
+
+def plot_ra_dec_inner(dataframe: pd.DataFrame, ax, dots_per_sqdeg, ra_min, ra_max, dec_min, dec_max, split, alpha):
+
+    if split:
+        obs = dataframe[dataframe.z_assigned_flag == 0]
+        unobs = dataframe[dataframe.z_assigned_flag != 0]
+    else:
+        obs = dataframe
+
+    # 8 sq deg / 5000 fibers is 0.0016 sq deg per fiber
+    # But in reality the paper says 0.0019 sq deg is area of a fiber's region (there is some overlap)
+    # That is the size we want to draw for each galaxy here.
+    # For 10,000 dots per sq degree, 0.0019 * 10000 = 
+    fiber_patrol_area = 0.00191 # in sq deg
+    ARBITRARY_SCALE_UP = 1 # my calculation didn't work so arbitrarilly sizing up with this
+    size = fiber_patrol_area * dots_per_sqdeg * ARBITRARY_SCALE_UP 
+
+    obs_selected = obs.query('RA < @ra_max and RA > @ra_min and Dec < @dec_max and Dec > @dec_min')
+    ax.scatter(obs_selected.RA, obs_selected.Dec, s=size, alpha=alpha)
+    if split:
+        unobs_selected = unobs.query('RA < @ra_max and RA > @ra_min and Dec < @dec_max and Dec > @dec_min')
+        ax.scatter(unobs_selected.RA, unobs_selected.Dec, marker='x', s=size, alpha=alpha)
+
+
 
 def _getsize(z):
     if z < 0.05: