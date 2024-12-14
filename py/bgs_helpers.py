Refactor and trying to fix/understand wp calculation issues.

py/bgs_helpers.py
@@ -5,16 +5,61 @@ import astropy.units as u
 import astropy.io.fits as fits
 from astropy.table import Table,join,vstack,unique,QTable
 import sys
+import pickle
 
 if './SelfCalGroupFinder/py/' not in sys.path:
     sys.path.append('./SelfCalGroupFinder/py/')
 from pyutils import *
 from dataloc import *
-from photoz import *
-from groupcatalog import tile_to_region, sv3_poor_y3overlap
+
+# I built this list of tiles by looking at https://www.legacysurvey.org/viewer-desi and viewing DESI EDR tiles (look for SV3)
+sv3_regions = [
+    [122, 128, 125, 124, 120, 127, 126, 121, 123, 129],
+    [499, 497, 503, 500, 495, 502, 501, 496, 498, 504],
+    [14,  16,  20,  19,  13,  12,  21,  18,  15,  17 ],
+    [41,  47,  49,  44,  43,  39,  46,  45,  40,  42,  48],
+    [68,  74,  76,  71,  70,  66,  73,  72,  67,  69,  75],
+    [149, 155, 152, 147, 151, 154, 148, 156, 150, 153], 
+    [527, 533, 530, 529, 525, 532, 531, 526, 528, 534], 
+    [236, 233, 230, 228, 238, 234, 232, 231, 235, 237, 229],
+    [265, 259, 257, 262, 263, 256, 260, 264, 255, 258, 261],
+    [286, 284, 289, 290, 283, 287, 291, 282, 285, 288],
+    [211, 205, 203, 208, 209, 202, 206, 210, 201, 204, 207],
+    [397, 394, 391, 400, 399, 392, 393, 398, 396, 395, 390],
+    [373, 365, 371, 367, 368, 363, 369, 370, 366, 364, 372],
+    [346, 338, 340, 344, 343, 341, 336, 342, 339, 337, 345],
+    [592, 589, 586, 595, 587, 593, 590, 594, 585, 588, 591],
+    [313, 316, 319, 311, 317, 314, 309, 310, 318, 312, 315],
+    [176, 182, 184, 179, 178, 174, 181, 180, 175, 177, 183],
+    [564, 558, 556, 561, 562, 555, 559, 560, 565, 563, 557],
+    [421, 424, 427, 419, 425, 422, 417, 423, 418, 420, 426],
+    [95,  101, 103, 98,  97,  93,  100, 99,  94,  96,  102],
+]
+sv3_poor_y3overlap = [0,1] # the first two regions from above have poor overlap wth Y3 footprint
+
+sv3_regions_sorted = []
+for region in sv3_regions:
+    a = region.copy()
+    a.sort()
+    sv3_regions_sorted.append(a)
+
+# Build a dictionary of tile_id to region index
+sv3_tile_to_region = {}
+for i, region in enumerate(sv3_regions):
+    for tile in region:
+        sv3_tile_to_region[tile] = i
+def tile_to_region_raw(key):
+    return sv3_tile_to_region.get(key, None)  # Return None if key is not found
+tile_to_region = np.vectorize(tile_to_region_raw)
 
 # This corresponds to 8.33 square degrees and empircally makes sense by looking at the randoms
 TILE_RADIUS = 5862.0 * u.arcsec # arcsec
+
+# Sentinal value from legacy survey for no photo-z
+NO_PHOTO_Z = -99.0
+
+NTILE_MIN_TO_FIND = 10
+
 
 def find_tiles_for_galaxies(tiles_df, gals_df, num_tiles_to_find):
     num_galaxies = len(gals_df.RA)
@@ -226,95 +271,116 @@ def fix_columns_in_phot_z_file(f):
 # Run this after building photo-z file
 #fix_columns_in_phot_z_file(IAN_PHOT_Z_FILE_NOSPEC)
 
+def read_randoms(base, n):
+    rtable = Table.read(base.replace("X", str(n)), format='fits')
+    r_dec = rtable['DEC'].astype("<f8")
+    r_ra = rtable['RA'].astype("<f8")
+    r_ntiles = rtable['NTILE'].astype("<i8")
 
+    randoms_df = pd.DataFrame({'RA': r_ra, 'Dec': r_dec, 'NTILE': r_ntiles})
+
+    if 'WEIGHT' in rtable.columns:
+        randoms_df['WEIGHT'] = rtable['WEIGHT'].astype("<f8")
+
+    return randoms_df
+
+def read_randoms_with_addons(base, n, tiles_df, apply_10p_cut):
+    randoms_df = read_randoms(base, n)
+    ntiles_inside, nearest_tile_ids = find_tiles_for_galaxies(tiles_df, randoms_df, NTILE_MIN_TO_FIND)
+    randoms_df['NTILE_MINE'] = ntiles_inside
+    randoms_df['REGION'] = tile_to_region(nearest_tile_ids[:, 0])
+    if apply_10p_cut:
+        randoms_df = randoms_df[randoms_df['NTILE_MINE'] >= 10]
+    return randoms_df
+
+def get_sv3_randoms_inner():
+    r = pickle.load(open(MY_RANDOMS_SV3, "rb"))
+    return r[r['NTILE_MINE'] >= 10]
+        
+def get_sv3_randoms_raw():
+    return pickle.load(open(MY_RANDOMS_SV3, "rb"))
+
+def get_sv3_randoms_inner_mini():
+    r = pickle.load(open(MY_RANDOMS_SV3_MINI, "rb"))
+    return r[r['NTILE_MINE'] >= 10]
+
+def get_sv3_randoms_raw_mini():
+    return pickle.load(open(MY_RANDOMS_SV3_MINI, "rb"))
+    
+def build_sv3footprint_randoms_files(randoms_file, mini_output_file, mini_output_file_filtered, big_output_file, big_output_file_filtered, has_NS_split=False, apply_10p_cut=False):
+    tiles_sv3 = read_tiles_Y3_sv3() # Gets the SV3 bright tiles from the Y3 master tile list
+
+    # Make a mini version from one randoms file
+    randoms_df0 = read_randoms_with_addons(randoms_file, 0, tiles_sv3, apply_10p_cut)
+    pickle.dump(randoms_df0, open(mini_output_file, "wb"))
+    to_remove = np.isin(randoms_df0['REGION'], sv3_poor_y3overlap)
+    randoms_df0 = randoms_df0.loc[~to_remove]
+    pickle.dump(randoms_df0, open(mini_output_file_filtered, "wb"))
+
+    # Make a big version combining all randoms files
+    big_rand_df = read_randoms_with_addons(randoms_file, 0, tiles_sv3, apply_10p_cut)
+    for i in range(1, 18):
+        rand_df = read_randoms_with_addons(randoms_file, i, tiles_sv3, apply_10p_cut)
+        big_rand_df = pd.concat([big_rand_df, rand_df])
+
+    if has_NS_split:
+        for i in range(0, 18):
+            rand_df = read_randoms_with_addons(randoms_file.replace("_N_", "_S_"), i, tiles_sv3, apply_10p_cut)
+            big_rand_df = pd.concat([big_rand_df, rand_df])
+
+    pickle.dump(big_rand_df, open(big_output_file, "wb"))
+    
+    to_remove = np.isin(big_rand_df['REGION'], sv3_poor_y3overlap)
+    big_rand_df = big_rand_df.loc[~to_remove]
+    pickle.dump(big_rand_df, open(big_output_file_filtered, "wb"))
 
 def build_sv3_full_randoms_files():
-    NTILE_MIN = 10
-
-    def read_randoms(base, n):
-        rtable = Table.read(base.replace("X", str(n)), format='fits')
-        r_dec = rtable['DEC'].astype("<f8")
-        r_ra = rtable['RA'].astype("<f8")
-        r_ntiles = rtable['NTILE'].astype("<i8")
-
-        randoms_df = pd.DataFrame({'RA': r_ra, 'Dec': r_dec, 'NTILE': r_ntiles})
-        return randoms_df
-    
-    tiles_BGS = read_tiles_Y3_sv3()
-    
-    # Make a mini version from one randoms file
-    randoms_df0 = read_randoms(BGS_SV3_RAND_FILE, 0)
-    
-    ntiles_inside, nearest_tile_ids = find_tiles_for_galaxies(tiles_BGS, randoms_df0, NTILE_MIN)
-    randoms_df0['NTILE_MINE'] = ntiles_inside
-    randoms_df0['REGION'] = tile_to_region(nearest_tile_ids[:,0])
-    pickle.dump(randoms_df0, open(MY_RANDOMS_SV3_MINI_20, "wb"))
-    to_remove = np.isin(randoms_df0['REGION'], sv3_poor_y3overlap)
-    randoms_df0 = randoms_df0.loc[~to_remove]
-    pickle.dump(randoms_df0, open(MY_RANDOMS_SV3_MINI, "wb"))
-    
-    
-    # Make a big version combining all randoms files
-    big_rand_df = read_randoms(BGS_SV3_RAND_FILE, 0)
-    for i in range(1, 18):
-        rand_df = read_randoms(BGS_SV3_RAND_FILE, i)
-        big_rand_df = pd.concat([big_rand_df, rand_df])
-    
-    ntiles_inside, nearest_tile_ids = find_tiles_for_galaxies(tiles_BGS, big_rand_df, NTILE_MIN)
-    big_rand_df['NTILE_MINE'] = ntiles_inside
-    big_rand_df['REGION'] = tile_to_region(nearest_tile_ids[:,0])
-
-    pickle.dump(big_rand_df, open(MY_RANDOMS_SV3_20, "wb"))
-
-    to_remove = np.isin(big_rand_df['REGION'], sv3_poor_y3overlap)
-    big_rand_df = big_rand_df.loc[~to_remove]
-
-    pickle.dump(big_rand_df, open(MY_RANDOMS_SV3, "wb"))
-    
-    
+    build_sv3footprint_randoms_files(
+        BGS_SV3_RAND_FILE,
+        MY_RANDOMS_SV3_MINI_20,
+        MY_RANDOMS_SV3_MINI,
+        MY_RANDOMS_SV3_20,
+        MY_RANDOMS_SV3
+    )
 
 def build_sv3_clustering_randoms_files():
-    NTILE_MIN = 10
+    build_sv3footprint_randoms_files(
+        BGS_SV3_CLUSTERING_RAND_FILE,
+        MY_RANDOMS_SV3_CLUSTERING_MINI_20,
+        MY_RANDOMS_SV3_CLUSTERING_MINI,
+        MY_RANDOMS_SV3_CLUSTERING_20,
+        MY_RANDOMS_SV3_CLUSTERING,
+        has_NS_split=True
+    )
 
-    def read_randoms(base, n):
-        rtable = Table.read(base.replace("X", str(n)), format='fits')
-        r_dec = rtable['DEC'].astype("<f8")
-        r_ra = rtable['RA'].astype("<f8")
-        r_ntiles = rtable['NTILE'].astype("<i8")
+def build_y3_likesv3_clustering_randoms_files():
+    build_sv3footprint_randoms_files(
+        BGS_Y3_CLUSTERING_RAND_FILE,
+        MY_RANDOMS_Y3_LIKESV3_CLUSTERING_MINI_20,
+        MY_RANDOMS_Y3_LIKESV3_CLUSTERING_MINI,
+        MY_RANDOMS_Y3_LIKESV3_CLUSTERING_20,
+        MY_RANDOMS_Y3_LIKESV3_CLUSTERING,
+        apply_10p_cut=True # Do this here instead of later to reduce file
+    )
 
-        randoms_df = pd.DataFrame({'RA': r_ra, 'Dec': r_dec, 'NTILE': r_ntiles})
-        return randoms_df
-    
-    tiles_BGS = read_tiles_Y3_sv3()
-    
+def build_y1_randoms_files():
+    tiles = read_tiles_Y1_main()
+
     # Make a mini version from one randoms file
-    randoms_df0 = read_randoms(BGS_SV3_CLUSTERING_RAND_FILE, 0)
-    
-    ntiles_inside, nearest_tile_ids = find_tiles_for_galaxies(tiles_BGS, randoms_df0, NTILE_MIN)
+    randoms_df0 = read_randoms(BGS_Y1_RAND_FILE, 0)
+    ntiles_inside, nearest_tile_ids = find_tiles_for_galaxies(tiles, randoms_df0, NTILE_MIN_TO_FIND)
     randoms_df0['NTILE_MINE'] = ntiles_inside
-    randoms_df0['REGION'] = tile_to_region(nearest_tile_ids[:,0])
-    pickle.dump(randoms_df0, open(MY_RANDOMS_SV3_CLUSTERING_MINI_20, "wb"))
-    to_remove = np.isin(randoms_df0['REGION'], sv3_poor_y3overlap)
-    randoms_df0 = randoms_df0.loc[~to_remove]
-    pickle.dump(randoms_df0, open(MY_RANDOMS_SV3_CLUSTERING_MINI, "wb"))
-    
-    
-    # Make a big version combining all randoms files
-    big_rand_df = read_randoms(BGS_SV3_CLUSTERING_RAND_FILE, 0)
-    for i in range(1, 18):
-        rand_df = read_randoms(BGS_SV3_CLUSTERING_RAND_FILE, i)
-        big_rand_df = pd.concat([big_rand_df, rand_df])
-    for i in range(0, 18):
-        rand_df = read_randoms(BGS_SV3_CLUSTERING_RAND_FILE.replace("_N_", "_S_"), i)
-        big_rand_df = pd.concat([big_rand_df, rand_df])
-    
-    ntiles_inside, nearest_tile_ids = find_tiles_for_galaxies(tiles_BGS, big_rand_df, NTILE_MIN)
-    big_rand_df['NTILE_MINE'] = ntiles_inside
-    big_rand_df['REGION'] = tile_to_region(nearest_tile_ids[:,0])
+    pickle.dump(randoms_df0, open(MY_RANDOMS_Y1_MINI, "wb"))
 
-    pickle.dump(big_rand_df, open(MY_RANDOMS_SV3_CLUSTERING_20, "wb"))
+    # Full version may be needed later but not for now
 
-    to_remove = np.isin(big_rand_df['REGION'], sv3_poor_y3overlap)
-    big_rand_df = big_rand_df.loc[~to_remove]
+def build_y3_randoms_files():
+    tiles = read_tiles_Y3_main()
 
-    pickle.dump(big_rand_df, open(MY_RANDOMS_SV3_CLUSTERING, "wb"))
+    # Make a mini version from one randoms file
+    randoms_df0 = read_randoms(BGS_Y3_RAND_FILE, 0)
+    ntiles_inside, nearest_tile_ids = find_tiles_for_galaxies(tiles, randoms_df0, NTILE_MIN_TO_FIND)
+    randoms_df0['NTILE_MINE'] = ntiles_inside
+    pickle.dump(randoms_df0, open(MY_RANDOMS_Y3_MINI, "wb"))
+
+    # Full version may be needed later but not for now

py/groupcatalog.py
@@ -21,12 +21,10 @@ from pyutils import *
 from redshift_guesser import SimpleRedshiftGuesser, PhotometricRedshiftGuesser
 from hdf5_to_dat import pre_process_mxxl
 from uchuu_to_dat import pre_process_uchuu
+from bgs_helpers import *
 
 # Sentinal value for no truth redshift
 NO_TRUTH_Z = -99.99
-
-# Sentinal value from legacy survey for no photo-z
-NO_PHOTO_Z = -99.0
 
 # Shared bins for various purposes
 Mhalo_bins = np.logspace(10, 15.5, 40)
@@ -40,46 +38,6 @@ mstar_labels = mstar_bins[0:len(mstar_bins)-1]
 
 Mr_gal_bins = log_solar_L_to_abs_mag_r(np.log10(L_gal_bins))
 Mr_gal_labels = log_solar_L_to_abs_mag_r(np.log10(L_gal_labels))
-
-# I built this list of tiles by looking at https://www.legacysurvey.org/viewer-desi and viewing DESI EDR tiles (look for SV3)
-sv3_regions = [
-    [122, 128, 125, 124, 120, 127, 126, 121, 123, 129],
-    [499, 497, 503, 500, 495, 502, 501, 496, 498, 504],
-    [14,  16,  20,  19,  13,  12,  21,  18,  15,  17 ],
-    [41,  47,  49,  44,  43,  39,  46,  45,  40,  42,  48],
-    [68,  74,  76,  71,  70,  66,  73,  72,  67,  69,  75],
-    [149, 155, 152, 147, 151, 154, 148, 156, 150, 153], 
-    [527, 533, 530, 529, 525, 532, 531, 526, 528, 534], 
-    [236, 233, 230, 228, 238, 234, 232, 231, 235, 237, 229],
-    [265, 259, 257, 262, 263, 256, 260, 264, 255, 258, 261],
-    [286, 284, 289, 290, 283, 287, 291, 282, 285, 288],
-    [211, 205, 203, 208, 209, 202, 206, 210, 201, 204, 207],
-    [397, 394, 391, 400, 399, 392, 393, 398, 396, 395, 390],
-    [373, 365, 371, 367, 368, 363, 369, 370, 366, 364, 372],
-    [346, 338, 340, 344, 343, 341, 336, 342, 339, 337, 345],
-    [592, 589, 586, 595, 587, 593, 590, 594, 585, 588, 591],
-    [313, 316, 319, 311, 317, 314, 309, 310, 318, 312, 315],
-    [176, 182, 184, 179, 178, 174, 181, 180, 175, 177, 183],
-    [564, 558, 556, 561, 562, 555, 559, 560, 565, 563, 557],
-    [421, 424, 427, 419, 425, 422, 417, 423, 418, 420, 426],
-    [95,  101, 103, 98,  97,  93,  100, 99,  94,  96,  102],
-]
-sv3_poor_y3overlap = [0,1] # the first two regions from above have poor overlap wth Y3 footprint
-
-sv3_regions_sorted = []
-for region in sv3_regions:
-    a = region.copy()
-    a.sort()
-    sv3_regions_sorted.append(a)
-
-# Build a dictionary of tile_id to region index
-sv3_tile_to_region = {}
-for i, region in enumerate(sv3_regions):
-    for tile in region:
-        sv3_tile_to_region[tile] = i
-def tile_to_region_raw(key):
-    return sv3_tile_to_region.get(key, None)  # Return None if key is not found
-tile_to_region = np.vectorize(tile_to_region_raw)
 
 CLUSTERING_MAG_BINS = [-16, -17, -18, -19, -20, -21, -22, -23]
 
@@ -770,15 +728,15 @@ class BGSGroupCatalog(GroupCatalog):
 
         print("Post-processing done.")
 
-    def get_clustering_randoms(self):
+    def get_randoms(self):
         if self.data_cut == 'sv3' or self.data_cut == 'Y3-Kibo-SV3Cut' or self.data_cut == 'Y3-Loa-SV3Cut':
-            return pickle.load(open(MY_RANDOMS_SV3_CLUSTERING, "rb"))
+            return get_sv3_randoms_inner()
         else:
             print("Randoms not available for this data cut.")
         
-    def get_clustering_randoms_mini(self):
+    def get_randoms_mini(self):
         if self.data_cut == 'sv3' or self.data_cut == 'Y3-Kibo-SV3Cut' or self.data_cut == 'Y3-Loa-SV3Cut':
-            return pickle.load(open(MY_RANDOMS_SV3_CLUSTERING_MINI, "rb"))
+            return get_sv3_randoms_inner_mini()
         else:
             print("Randoms not available for this data cut.")
 
@@ -792,14 +750,14 @@ class BGSGroupCatalog(GroupCatalog):
             df['mag_bin'] = np.digitize(df.mag_R, CLUSTERING_MAG_BINS)
 
         if with_extra_randoms:
-            randoms = self.get_clustering_randoms()
+            randoms = self.get_randoms() # TODO clustering or full randoms???
         else:
-            randoms = self.get_clustering_randoms_mini()
+            randoms = self.get_randoms_mini() # TODO clustering or full randoms???
 
         if randoms is not None:
             print(f"Calculating mini projected clustering: Random count / data count = {len(randoms['RA'])} / {len(df)}")
             if with_extra_randoms:
-                self.wp_all_extra = wp.calculate_wp_from_df(df, randoms)
+                self.wp_all_extra = wp.calculate_wp_from_df(df, randoms) 
             else:
                 self.wp_all = wp.calculate_wp_from_df(df, randoms)
         else:
@@ -831,11 +789,9 @@ class BGSGroupCatalog(GroupCatalog):
             df['mag_bin'] = np.digitize(df.mag_R, CLUSTERING_MAG_BINS)
 
         if with_extra_randoms:
-            randoms = self.get_clustering_randoms()
-            # For some reason this code path hangs... one loop takes way longer than
-            # the full sample with the same number of randoms.
+            randoms = self.get_randoms()# TODO clustering or full randoms???
         else:
-            randoms = self.get_clustering_randoms_mini()
+            randoms = self.get_randoms_mini()# TODO clustering or full randoms???
 
         for i in range(len(CLUSTERING_MAG_BINS)):
             txt = "extra" if with_extra_randoms else "mini"
@@ -870,9 +826,9 @@ class BGSGroupCatalog(GroupCatalog):
         
         df = self.all_data
         if with_extra_randoms:
-            randoms = pickle.load(open(MY_RANDOMS_SV3, "rb"))
+            randoms = self.get_randoms()
         else:
-            randoms = pickle.load(open(MY_RANDOMS_SV3_MINI, "rb"))
+            randoms = self.get_randoms_mini()
 
         # label the SV3 region each galaxy is in
         df['region'] = tile_to_region(df['nearest_tile_id']).astype(int)
@@ -955,12 +911,15 @@ class BGSGroupCatalog(GroupCatalog):
         super().refresh_df_views()
         self.add_bootstrapped_f_sat()
 
-    def write_sharable_output_file(self):
+    def write_sharable_output_file(self, name=None):
         print("Writing a sharable output file")
-        filename_out = str.replace(self.GF_outfile, ".out", " Catalog.csv")
+        if name is None:
+            name = str.replace(self.GF_outfile, ".out", " Catalog.csv")
+        elif not name.endswith('.csv'):
+            name = name + '.csv'
         df = self.all_data.drop(columns=['Mstar_bin', 'Mh_bin', 'Lgal_bin', 'logLgal', 'Dn4000'])
         print(df.columns)
-        df.to_csv(filename_out, index=False, header=True)
+        df.to_csv(name, index=False, header=True)
 
 
 def get_extra_bgs_fastspectfit_data():
@@ -1127,17 +1086,15 @@ def get_footprint_fraction(data_cut, mode, num_passes_required):
         FOOTPRINT_FRAC_4pass = 0.0228093 # 940 degrees
         # 0% 5pass coverage
     elif data_cut == "Y3-Jura":
-        # For Y3-Jura
         FOOTPRINT_FRAC_1pass = 0.310691 # 12816 degrees
         FOOTPRINT_FRAC_2pass = 0.286837 # 11832 degrees
         FOOTPRINT_FRAC_3pass = 0.233920 # 9649 degrees
         FOOTPRINT_FRAC_4pass = 0.115183 # 4751 degrees
     elif data_cut == "Y3-Kibo" or data_cut == "Y3-Loa":
-        # For Y3-Kibo
-        FOOTPRINT_FRAC_1pass = 0.3112278 # 12839 degrees
-        FOOTPRINT_FRAC_2pass = 0.2870291 # 11840 degrees
-        FOOTPRINT_FRAC_3pass = 0.2338243 # 9645 degrees
-        FOOTPRINT_FRAC_4pass = 0.1150345 # 4745 degrees
+        FOOTPRINT_FRAC_1pass = 0.30968189465008605 # 12775 degrees
+        FOOTPRINT_FRAC_2pass = 0.2859776210215015 # 11797 degrees
+        FOOTPRINT_FRAC_3pass = 0.23324031706784962 # 9621 degrees
+        FOOTPRINT_FRAC_4pass = 0.1148695997866822 # 4738 degrees
     elif data_cut == "sv3":
         # These are for the 18/20 patches being used. We dropped two due to poor Y3 overlap.
         FOOTPRINT_FRAC_1pass =  156.2628 / DEGREES_ON_SPHERE 

py/nnanalysis.py
@@ -262,7 +262,7 @@ class NNAnalyzer_cic():
         to_match = coord.SkyCoord(ra=df.loc[rl,'ra'].to_numpy()*u.degree, dec=df.loc[rl,'dec'].to_numpy()*u.degree, frame='icrs')
         idx, d2d, d3d = coord.match_coordinates_sky(to_match, catalog, nthneighbor=nthneighbor, storekdtree=False)
         ang_dist = d2d.to(u.arcsec).value
-        sim_z = close_enough_smooth(df.loc[rl,'z_obs'], z_obs_catalog[idx])
+        sim_z = rounded_tophat_score(df.loc[rl,'z_obs'], z_obs_catalog[idx])
 
         df['nn1_ang_dist'] = np.nan
         df.loc[rl,'nn1_ang_dist'] = ang_dist

py/plotting.py
@@ -352,11 +352,11 @@ def plots(*catalogs, show_err=None, truth_on=False):
         plots_color_split(*catalogs, total_on=True)
 
 
-def wp_rp(catalog: GroupCatalog):
+def wp_rp(catalog: GroupCatalog|tuple):
     colors = ['k', 'r', 'b']
     f = catalog
     plt.figure(figsize=(5, 5))
-    to_use = f.get_best_wp_all()
+    to_use = f.get_best_wp_all() if isinstance(f, GroupCatalog) else f
     if hasattr(f, 'wp_err'):
         plt.errorbar(to_use[0][:-1], to_use[1], yerr=f.wp_err, marker='o', linestyle='-', label='All', color=colors[0])
         plt.errorbar(to_use[0][:-1], to_use[2], yerr=f.wp_r_err, marker='o', linestyle='-', label='Red', color=colors[1])
@@ -371,7 +371,8 @@ def wp_rp(catalog: GroupCatalog):
     plt.xlabel(r'$r_p$ [Mpc/h]')
     plt.ylabel(r'$w_p(r_p)$')
     plt.legend()
-    plt.title(f'{catalog.name} Full $w_p(r_p)$ ')
+    if isinstance(catalog, BGSGroupCatalog):
+        plt.title(f'{catalog.name} Full $w_p(r_p)$') 
     plt.grid(True)
     plt.show()
 
@@ -412,53 +413,65 @@ def wp_rp_magbins(c: GroupCatalog):
     plt.show()
 
 
-def compare_wp_rp(d1: BGSGroupCatalog, d2_t: BGSGroupCatalog):
-    def plot_fractional_difference(ax, wp1, wp2, wp1_red, wp2_red, wp1_blue, wp2_blue, label):
+def compare_wp_rp(d1: BGSGroupCatalog|tuple, d2_t: BGSGroupCatalog|tuple):
+    """
+    Compare two wp_rp functions (total, red, blue). The second is used as the 'truth' (in the denominator).
+    """
+    
+    def plot_fractional_difference(ax, wp1, wp2, wp1_red, wp2_red, wp1_blue, wp2_blue, label, bins):
         percent_diff = 100 * (wp1 - wp2) / wp2
         percent_diff_r = 100 * (wp1_red - wp2_red) / wp2_red
         percent_diff_b = 100 * (wp1_blue - wp2_blue) / wp2_blue
 
-        ax.plot(d1.get_best_wp_all()[0][:-1], percent_diff, marker='o', linestyle='-', color='black', label=f'Overall {label}')
-        ax.plot(d1.get_best_wp_all()[0][:-1], percent_diff_r, marker='o', linestyle='-', color='red', label=f'Red {label}')
-        ax.plot(d1.get_best_wp_all()[0][:-1], percent_diff_b, marker='o', linestyle='-', color='blue', label=f'Blue {label}')
+        ax.plot(bins, percent_diff, marker='o', linestyle='-', color='black', label=f'Overall {label}')
+        ax.plot(bins, percent_diff_r, marker='o', linestyle='-', color='red', label=f'Red {label}')
+        ax.plot(bins, percent_diff_b, marker='o', linestyle='-', color='blue', label=f'Blue {label}')
         
         ax.set_xscale('log')
-        ax.set_ylim(-25, 25)
+        lim = np.max([*np.abs(percent_diff), *np.abs(percent_diff_r), *np.abs(percent_diff_b)])
+        ax.set_ylim(-lim, lim)
         ax.set_yscale('linear')
         ax.set_xlabel(r'$r_p$ [Mpc/h]')
         ax.set_ylabel(r'$w_p(r_p)$ Difference (%)')
         ax.grid(True)
 
     fig, ax = plt.subplots(figsize=(5, 5))
-    plot_fractional_difference(ax, d1.get_best_wp_all()[1], d2_t.get_best_wp_all()[1], d1.get_best_wp_all()[2], d2_t.get_best_wp_all()[2], d1.get_best_wp_all()[3], d2_t.get_best_wp_all()[3], 'Flux-limited')
+    first = d1.get_best_wp_all() if isinstance(d1, GroupCatalog) else d1
+    second = d2_t.get_best_wp_all() if isinstance(d2_t, GroupCatalog) else d2_t
+
+    plot_fractional_difference(ax, first[1], second[1], first[2], second[2], first[3], second[3], 'Flux-limited', first[0][:-1])
     
     # Show a shaded region for the error in the d2 set. 
-    #ax.fill_between(d2_t.get_best_wp_all()[0][:-1], 100*d2_t.wp_err/d2_t.get_best_wp_all()[1], -100*d2_t.wp_err/d2_t.get_best_wp_all()[1], color='black', alpha=0.2)
-    ax.fill_between(d2_t.get_best_wp_all()[0][:-1], 100*d2_t.wp_r_err/d2_t.get_best_wp_all()[2], -100*d2_t.wp_r_err/d2_t.get_best_wp_all()[2], color='red', alpha=0.25)
-    ax.fill_between(d2_t.get_best_wp_all()[0][:-1], 100*d2_t.wp_b_err/d2_t.get_best_wp_all()[3], -100*d2_t.wp_b_err/d2_t.get_best_wp_all()[3], color='blue', alpha=0.25)
-    
-    ax.set_title(f'$\Delta w_p(r_p)$ - {d1.name}')
+    if hasattr(d2_t, 'wp_r_err') and hasattr(d2_t, 'wp_b_err'):
+        #ax.fill_between(d2_t.get_best_wp_all()[0][:-1], 100*d2_t.wp_err/d2_t.get_best_wp_all()[1], -100*d2_t.wp_err/d2_t.get_best_wp_all()[1], color='black', alpha=0.2)
+        ax.fill_between(d2_t.get_best_wp_all()[0][:-1], 100*d2_t.wp_r_err/d2_t.get_best_wp_all()[2], -100*d2_t.wp_r_err/d2_t.get_best_wp_all()[2], color='red', alpha=0.25)
+        ax.fill_between(d2_t.get_best_wp_all()[0][:-1], 100*d2_t.wp_b_err/d2_t.get_best_wp_all()[3], -100*d2_t.wp_b_err/d2_t.get_best_wp_all()[3], color='blue', alpha=0.25)
+        
+    if isinstance(d1, GroupCatalog) and isinstance(d2_t, GroupCatalog):
+        ax.set_title(f'{d1.name} vs {d2_t.name}')
     plt.legend()
-
     plt.tight_layout()
     plt.show()
     
-    # Additional rows for each magnitude slice in wp_slices
-    fig, axes = plt.subplots(2, 3, figsize=(12, 9))
 
-    for i, mag_slice in enumerate(d1.wp_slices):
-        if i <= 1:
-            continue
+    if isinstance(d1, BGSGroupCatalog) and isinstance(d2_t, BGSGroupCatalog):
+        
+        # Additional rows for each magnitude slice in wp_slices
+        fig, axes = plt.subplots(2, 3, figsize=(12, 9))
 
-        mag_range_label = f"{d1.wp_slices[i][4]:.1f} > $M_r$ - 5log($h$) > {d1.wp_slices[i][5]:.1f}"
-        row = (i - 2) // 3
-        col = (i - 2) % 3
+        for i, mag_slice in enumerate(d1.wp_slices):
+            if i <= 1:
+                continue
 
-        plot_fractional_difference(axes[row, col], d1.wp_slices[i][1], d2_t.wp_slices[i][1], d1.wp_slices[i][2], d2_t.wp_slices[i][2], d1.wp_slices[i][3], d2_t.wp_slices[i][3], f'\n{mag_range_label}')
-        axes[row, col].set_title(f'{mag_range_label}')
+            mag_range_label = f"{d1.wp_slices[i][4]:.1f} > $M_r$ - 5log($h$) > {d1.wp_slices[i][5]:.1f}"
+            row = (i - 2) // 3
+            col = (i - 2) % 3
 
-    plt.tight_layout()
-    plt.show()
+            plot_fractional_difference(axes[row, col], d1.wp_slices[i][1], d2_t.wp_slices[i][1], d1.wp_slices[i][2], d2_t.wp_slices[i][2], d1.wp_slices[i][3], d2_t.wp_slices[i][3], f'\n{mag_range_label}', d1.wp_slices[i][0][:-1])
+            axes[row, col].set_title(f'{mag_range_label}')
+
+        plt.tight_layout()
+        plt.show()
 
 
 def plots_color_split_lost_split(f, grpby_col):
@@ -1010,13 +1023,15 @@ def Lfunc_compare(cat1, cat2):
 def luminosity_function_plots(*catalogs):
     
     #x = L_gal_labels # bins if ising cic code
-    x = np.logspace(8, 11, 16) # bins if using cic code
+    x = np.logspace(8, 11, 12) # bins if using cic code
 
     obs_counts = np.zeros((len(catalogs), len(x)))
     lost_truth_counts = np.zeros((len(catalogs), len(x)))
     lost_assumed_counts = np.zeros((len(catalogs), len(x)))
     obs_vs_losttruth = np.zeros((len(catalogs), len(x)))
     assumed_vs_truth = np.zeros((len(catalogs), len(x)))
+    assumed_vs_truth_red = np.zeros((len(catalogs), len(x)))
+    assumed_vs_truth_blue = np.zeros((len(catalogs), len(x)))
     total_counts = np.zeros((len(catalogs), len(x)))
     total_r_counts = np.zeros((len(catalogs), len(x)))
     total_b_counts = np.zeros((len(catalogs), len(x)))
@@ -1064,8 +1079,8 @@ def luminosity_function_plots(*catalogs):
         lost_assumed_red_counts = nn.cic_binning(lost_withT_galaxies.loc[lost_withT_galaxies.quiescent, 'L_gal'].to_numpy(), [x])
         lost_assumed_blue_counts = nn.cic_binning(lost_withT_galaxies.loc[~lost_withT_galaxies.quiescent, 'L_gal'].to_numpy(), [x])
 
-        assumed_vs_truth_red =  ((lost_assumed_red_counts - lost_truth_red_counts) / lost_truth_red_counts) * 100
-        assumed_vs_truth_blue =  ((lost_assumed_blue_counts - lost_truth_blue_counts) / lost_truth_blue_counts) * 100
+        assumed_vs_truth_red[i] =  ((lost_assumed_red_counts - lost_truth_red_counts) / lost_truth_red_counts) * 100
+        assumed_vs_truth_blue[i] =  ((lost_assumed_blue_counts - lost_truth_blue_counts) / lost_truth_blue_counts) * 100
 
         with np.printoptions(precision=0, suppress=True, linewidth=200):
             print(f"\n*** {catalogs[i].name} ***")
@@ -1080,6 +1095,7 @@ def luminosity_function_plots(*catalogs):
         plt.plot(x, total_r_counts[i], color='r', label='Total quiescent')
         plt.plot(x, total_b_counts[i], color='b', label='Total star-forming')
 
+        """
         plt.figure(dpi=DPI)
         plt.plot(x, obs_counts[i], color='b', label='Obs galaxies')
         plt.plot(x, lost_truth_counts[i]*boost[i], color='g', label='Lost gals (True)')
@@ -1091,11 +1107,11 @@ def luminosity_function_plots(*catalogs):
         plt.xlim(LGAL_MIN, LGAL_MAX)
         plt.xlabel('$L_{gal}$')
         plt.draw()
-
+        """
         # Split by quiescent and star-forming
         plt.figure(dpi=DPI)
-        plt.plot(x, assumed_vs_truth_red, color='r', label=f'Quiescent ({catalog.name})')
-        plt.plot(x, assumed_vs_truth_blue, color='b', label=f'Star-forming ({catalog.name})')
+        plt.plot(x, assumed_vs_truth_red[i], color='r', label=f'Quiescent ({catalog.name})')
+        plt.plot(x, assumed_vs_truth_blue[i], color='b', label=f'Star-forming ({catalog.name})')
         plt.title("Lost $\Phi_{Truth}(L)$ -> $\Phi_{Assumed}(L)$ - " + catalog.name)
         plt.xlabel('$L_{gal}$')
         plt.ylabel("% Change in counts")
@@ -1122,14 +1138,37 @@ def luminosity_function_plots(*catalogs):
     plt.figure(dpi=DPI)
     for i in range(len(catalogs)):
         plt.plot(x , assumed_vs_truth[i], label=f"{catalogs[i].name}", color=catalogs[i].color)
-    plt.title("Lost (truth) => Lost (assumed) Luminosity Function")
+    plt.title("Change in Lost Galaxy $\Phi_L(L)$")
     plt.xlabel('$L_{gal}$')
-    plt.ylabel("% Change in counts")
+    plt.ylabel("$\Delta \Phi_L(L)$ (%)")
     plt.xscale('log')
     plt.xlim(LGAL_MIN, LGAL_MAX)
     plt.ylim(-60, 60)
     plt.legend()
     plt.axhline(0, color='black', lw=1)
+    plt.draw()
+
+    fig, axes = plt.subplots(nrows = 1, ncols=2, figsize=(10, 4), dpi=DPI)
+    for i in range(len(catalogs)):
+        axes[0].plot(x , assumed_vs_truth_red[i], label=f"{catalogs[i].name}", color=catalogs[i].color)
+    #axes[0].set_title("Change in Red Lost Galaxy $\Phi_L(L)$")
+    axes[0].set_xlabel('$L_{gal}$')
+    axes[0].set_ylabel("$\Delta \Phi_{L,r}(L)$ (%)")
+    axes[0].set_xscale('log')
+    axes[0].set_xlim(LGAL_MIN, LGAL_MAX)
+    axes[0].set_ylim(-60, 60)
+    axes[0].axhline(0, color='black', lw=1)
+    for i in range(len(catalogs)):
+        axes[1].plot(x , assumed_vs_truth_blue[i], label=f"{catalogs[i].name}", color=catalogs[i].color)
+    #axes[1].set_title("Change in Blue Lost Galaxy $\Phi_L(L)$")
+    axes[1].set_xlabel('$L_{gal}$')
+    axes[1].set_ylabel("$\Delta \Phi_{L,b}(L)$ (%)")
+    axes[1].set_xscale('log')
+    axes[1].set_xlim(LGAL_MIN, LGAL_MAX)
+    axes[1].set_ylim(-60, 60)
+    axes[1].axhline(0, color='black', lw=1)
+    plt.legend()
+    fig.tight_layout()
     plt.draw()
 
 
@@ -1170,8 +1209,8 @@ def correct_redshifts_assigned_plot(*sets: GroupCatalog):
         truth_z = s.all_data.loc[idx, 'z_T'] # was using z_obs before which is fine for SV3 with droped passes
         assignment_type = s.all_data.loc[idx, 'z_assigned_flag']
 
-        score = sim_z_score(assigned_z, truth_z)
-        rtophat = close_enough_smooth(assigned_z, truth_z)
+        score = powerlaw_score_1(assigned_z, truth_z)
+        rtophat = rounded_tophat_score(assigned_z, truth_z)
         metric_score1 = photoz_plus_metric_1(assigned_z, truth_z, assignment_type)
         metric_score2 = photoz_plus_metric_2(assigned_z, truth_z, assignment_type)
         metric_score3 = photoz_plus_metric_3(assigned_z, truth_z, assignment_type)
@@ -1190,13 +1229,13 @@ def correct_redshifts_assigned_plot(*sets: GroupCatalog):
         for flag in np.unique(assignment_type):
             assigned_z_flag = assigned_z[assignment_type == flag]
             truth_z_flag = truth_z[assignment_type == flag]
-            rtophat_flag = close_enough_smooth(assigned_z_flag, truth_z_flag)
+            rtophat_flag = rounded_tophat_score(assigned_z_flag, truth_z_flag)
             print(f"Flag {AssignedRedshiftFlag(flag)} - {rtophat_flag.mean():.4f}")
 
         #print("Neighbor-assigned Only:")
         assigned_z2 = s.all_data.loc[valid_idx & z_flag_is_neighbor(s.all_data['z_assigned_flag']), 'z']
         observed_z2 = s.all_data.loc[valid_idx & z_flag_is_neighbor(s.all_data['z_assigned_flag']), 'z_T']
-        score2 = sim_z_score(assigned_z2, observed_z2)
+        score2 = rounded_tophat(assigned_z2, observed_z2)
         scores_n_only.append(score2.mean())
         #print(f" Galaxies to compare: {len(assigned_z2)} ({len(assigned_z2) / len(s.all_data):.1%})")
         #print(f" Score Mean: {score2.mean():.4f}")
@@ -1207,11 +1246,14 @@ def correct_redshifts_assigned_plot(*sets: GroupCatalog):
     width = 1/3  # the width of the bars; change based on how many bars you want
 
     fig, ax = plt.subplots(figsize=(6, 5), dpi=DPI)
-    rects1 = ax.bar(x - width, scores_all_lost, width, label='Score (Powerlaw kernel)', color=get_color(0))
-    #rects3 = ax.bar(x - width  , scores_n_only, width, label='% Powerlaw Score (N-assigned Only)', color=get_color(1))
-    rects2 = ax.bar(x          , rounded_tophat, width, label='% Correct (Tophat)', color=get_color(2))
+    #rects1 = ax.bar(x - width, scores_all_lost, width, label='Score (Powerlaw kernel)', color=get_color(0))
     #rects5 = ax.bar(x + width, scores_metric3, width, label='Metric 3', color=get_color(3))
-    #rects5 = ax.bar(x + width, scores_metric4, width, label='Metric 4', color=get_color(3))
+    
+    #rects2 = ax.bar(x - width/2        , rounded_tophat, width, label='Fraction Correct', color=get_color(2))
+    #rects5 = ax.bar(x + width/2, scores_metric4, width, label='MCMC Metric Score', color=get_color(3))
+
+    rects2 = ax.bar(x -width/2       , rounded_tophat, width, label='Fraction Correct (all)', color=get_color(2))
+    rects3 = ax.bar(x + width/2  , scores_n_only, width, label='Fraction Correct (neighbor assigned)', color=get_color(1))
 
     # Add some text for labels, title and custom x-axis tick labels, etc.
     #ax.set_xlabel('Catalogs')
@@ -1219,8 +1261,8 @@ def correct_redshifts_assigned_plot(*sets: GroupCatalog):
     ax.set_xticks(x)
     ax.set_xticklabels(labels, rotation=45, ha='right')
     ax.legend()
-    ax.set_ylim(0, 1.0)
-    ax.set_yticks(np.arange(0, 1.0, 0.1))
+    ax.set_ylim(0, 0.8)
+    ax.set_yticks(np.arange(0, 0.8, 0.1))
     ax.yaxis.grid(True)  # Add horizontal gridlines
 
     fig.tight_layout()

py/pyutils.py
@@ -201,7 +201,7 @@ def close_enough(target_z, z_arr, threshold=SIM_Z_THRESH):
     """
     return np.abs(z_arr - target_z) < threshold
 
-def close_enough_smooth(target_z, z_arr):
+def rounded_tophat_score(target_z, z_arr):
     """
     Compares two arrays of redshifts and returns a smooth transition from 1.0 to 0.0 around a 0.005 difference.
     This is used by the nnanalysis object to evaluat the similar-z fraction that goes into the bins.
@@ -217,7 +217,7 @@ def close_enough_smooth(target_z, z_arr):
     TURNING=0.0023
     return np.round(1.0 - erf((np.abs(z_arr-target_z) / (np.pi * TURNING))**TUNABLE), 3)
 
-def sim_z_score(target_z, z_arr):
+def powerlaw_score_1(target_z, z_arr):
     """
     Compares two arrays of redshifts. Instead of binary close enough evaluations like close_enough,
     this will give a 1.0 for close enough values and smoothly go to 0.0 for definintely far away targets.
@@ -252,7 +252,7 @@ def zdelta_spectrum(target_z, z_arr):
 def photoz_plus_metric_1(z_guessed: np.ndarray[float], z_truth: np.ndarray[float], guess_type: np.ndarray[int]):
     assert guess_type.shape == z_guessed.shape
     assert guess_type.shape == z_truth.shape
-    score = sim_z_score(z_guessed, z_truth)
+    score = powerlaw_score_1(z_guessed, z_truth)
 
     # pseudorandom guesses help preserve luminosity func so give them a little credit
     score = score + np.where(guess_type == AssignedRedshiftFlag.PSEUDO_RANDOM.value, 0.3, 0.0) 
@@ -267,7 +267,7 @@ def photoz_plus_metric_1(z_guessed: np.ndarray[float], z_truth: np.ndarray[float
 def photoz_plus_metric_2(z_guessed: np.ndarray[float], z_truth: np.ndarray[float], guess_type: np.ndarray[int]):
     assert guess_type.shape == z_guessed.shape
     assert guess_type.shape == z_truth.shape
-    score = sim_z_score(z_guessed, z_truth)
+    score = powerlaw_score_1(z_guessed, z_truth)
 
     # pseudorandom guesses help preserve luminosity func so give them a little credit
     score = score + np.where(guess_type == AssignedRedshiftFlag.PSEUDO_RANDOM.value, 0.4, 0.0) 
@@ -282,7 +282,7 @@ def photoz_plus_metric_2(z_guessed: np.ndarray[float], z_truth: np.ndarray[float
 def photoz_plus_metric_3(z_guessed: np.ndarray[float], z_truth: np.ndarray[float], guess_type: np.ndarray[int]):
     assert guess_type.shape == z_guessed.shape
     assert guess_type.shape == z_truth.shape
-    score = sim_z_score(z_guessed, z_truth)
+    score = powerlaw_score_1(z_guessed, z_truth)
 
     # pseudorandom guesses help preserve luminosity func so give them a little credit
     score = score + np.where(guess_type == AssignedRedshiftFlag.PSEUDO_RANDOM.value, 0.4, 0.0) 

py/wp.py
@@ -84,13 +84,13 @@ RBINS = np.logspace(np.log10(RMIN), np.log10(RMAX), NBINS + 1)
 
 
 
-def calculate_wp_from_df(df: pd.DataFrame, randoms, weights=None):
+def calculate_wp_from_df(df: pd.DataFrame, randoms, data_weights=None, rand_weights=None):
     """
     Calculate the projected correlation function wp(rp) for the total, red, and blue
     galaxies for the provided dataframe and randoms using our canonical set of parameters.
     """
     #cz = df.z.to_numpy().copy() * const.c.to('km/s').value # corrfunc has a bug with cz
-    print(df.z.to_numpy())
+    #print(df.z.to_numpy())
     dist = Planck13.comoving_distance(df.z.to_numpy()) # Mpc
     
     # Overall sample
@@ -100,25 +100,28 @@ def calculate_wp_from_df(df: pd.DataFrame, randoms, weights=None):
         dist.value, 
         randoms.RA.to_numpy(),
         randoms.Dec.to_numpy(),
-        weights1=weights)
+        data_weights=data_weights,
+        rand_weights=rand_weights)
     rbins, wp_red = calculate_wp(
         df.loc[df.quiescent].RA.to_numpy(),
         df.loc[df.quiescent].Dec.to_numpy(),
         dist.value[df.quiescent],
         randoms.RA.to_numpy(),
         randoms.Dec.to_numpy(),
-        weights1=weights)
+        data_weights=data_weights,
+        rand_weights=rand_weights)
     rbins, wp_blue = calculate_wp(
         df.loc[~df.quiescent].RA.to_numpy(),
         df.loc[~df.quiescent].Dec.to_numpy(),
         dist.value[~df.quiescent],
         randoms.RA.to_numpy(),
         randoms.Dec.to_numpy(),
-        weights1=weights)
+        data_weights=data_weights,
+        rand_weights=rand_weights)
 
     return (rbins, wp_all, wp_red, wp_blue)
 
-def calculate_wp(data_ra, data_dec, data_dist, rand_ra, rand_dec, rand_dist=None, weights1=None, weights2=None):
+def calculate_wp(data_ra, data_dec, data_dist, rand_ra, rand_dec, rand_dist=None, data_weights=None, rand_weights=None):
     """
     Calculate the projected correlation function wp(rp) for a given data set and randoms using 
     our canonical set of parameters.
@@ -143,9 +146,9 @@ def calculate_wp(data_ra, data_dec, data_dist, rand_ra, rand_dec, rand_dist=None
     if rand_dist is None:
         rand_dist = g.choice(data_dist, size=len(rand_ra), replace=True)
 
-    DD_counts = DDrppi_mocks(1, COSMOLOGY, NTHREADS, PIMAX, RBINS, data_ra, data_dec, data_dist, weights1=weights1, is_comoving_dist=True)
-    DR_counts = DDrppi_mocks(0, COSMOLOGY, NTHREADS, PIMAX, RBINS, data_ra, data_dec, data_dist, RA2=rand_ra, DEC2=rand_dec, CZ2=rand_dist, weights1=weights1, weights2=weights2, is_comoving_dist=True)
-    RR_counts = DDrppi_mocks(1, COSMOLOGY, NTHREADS, PIMAX, RBINS, rand_ra, rand_dec, rand_dist, weights1=weights2, is_comoving_dist=True)
+    DD_counts = DDrppi_mocks(1, COSMOLOGY, NTHREADS, PIMAX, RBINS, data_ra, data_dec, data_dist, weights1=data_weights, is_comoving_dist=True)
+    DR_counts = DDrppi_mocks(0, COSMOLOGY, NTHREADS, PIMAX, RBINS, data_ra, data_dec, data_dist, RA2=rand_ra, DEC2=rand_dec, CZ2=rand_dist, weights1=data_weights, weights2=rand_weights, is_comoving_dist=True)
+    RR_counts = DDrppi_mocks(1, COSMOLOGY, NTHREADS, PIMAX, RBINS, rand_ra, rand_dec, rand_dist, weights1=rand_weights, is_comoving_dist=True)
 
     # Convert pair counts to wp 
     wp = convert_rp_pi_counts_to_wp(len(data_ra), len(data_ra), len(rand_ra), len(rand_ra), DD_counts, DR_counts, DR_counts, RR_counts, NBINS, PIMAX)