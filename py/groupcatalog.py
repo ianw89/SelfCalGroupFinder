Some small fixes and code to sum up and save fsat values (for a variance estimation) from MCMC logs

py/groupcatalog.py
@@ -33,9 +33,6 @@ NO_TRUTH_Z = -99.99
 Mhalo_bins = np.logspace(10, 15.5, 40)
 Mhalo_labels = Mhalo_bins[0:len(Mhalo_bins)-1] 
 
-L_gal_bins = np.logspace(6, 12.5, 40)
-L_gal_labels = L_gal_bins[0:len(L_gal_bins)-1]
-
 mstar_bins = np.logspace(6, 13, 30)
 mstar_labels = mstar_bins[0:len(mstar_bins)-1]
 
@@ -918,7 +915,7 @@ class TestGroupCatalog(GroupCatalog):
         self.GF_props = {
             'zmin':0,
             'zmax':1.0,
-            'frac_area':4.0/DEGREES_ONs_SPHERE,
+            'frac_area':4.0/DEGREES_ON_SPHERE,
             'fluxlim':1,
             'color':0,
         }
@@ -1064,8 +1061,10 @@ class BGSGroupCatalog(GroupCatalog):
         self.centered = None # SV3 Centered version shortcut.
         self.extra_params = extra_params
         self.GF_props = gfprops
-        self.caldata = CalibrationData.SDSS_5bin(self.mag_cut, self.GF_props['frac_area'])
-        #self.caldata = CalibrationData.BGS_Y1_6bin(self.mag_cut, self.GF_props['frac_area'])
+        frac_area = get_footprint_fraction(data_cut, mode, num_passes)
+        self.GF_props['frac_area'] = frac_area
+        #self.caldata = CalibrationData.SDSS_5bin(self.mag_cut, self.GF_props['frac_area'])
+        self.caldata = CalibrationData.BGS_Y1_6bin(self.mag_cut, self.GF_props['frac_area'])
 
     @staticmethod
     def from_MCMC(reader: emcee.backends.HDFBackend, mode: Mode):
@@ -1077,8 +1076,7 @@ class BGSGroupCatalog(GroupCatalog):
         
         print(f"Using MCMC parameters: {p}")
 
-        gc = BGSGroupCatalog(f"BGS SV3 MCMC {mode_to_str(mode)}", mode, 19.5, 23.0, sdss_fill=False, num_passes=10, drop_passes=3, data_cut="sv3", extra_params=p)
-        gc.GF_props = GF_PROPS_BGS_VANILLA.copy()
+        gc = BGSGroupCatalog(f"BGS SV3 MCMC {mode_to_str(mode)}", mode, 19.5, 23.0, sdss_fill=False, num_passes=10, drop_passes=3, data_cut="sv3", gfprops=GF_PROPS_BGS_VANILLA.copy(), extra_params=p)
         if mode.value == Mode.PHOTOZ_PLUS_v1.value:
             gc.color = 'g'
         elif mode.value == Mode.PHOTOZ_PLUS_v2.value:
@@ -1542,6 +1540,10 @@ def update_properties_for_indices(idx, app_mag_r, app_mag_g, g_r_apparent, z_eff
     Updates the properties for the given indices in the arrays. This is used for lost galaxies when we assigned
     redshifts to them generally.
     """
+    # Ensure app_mag_r and z_eff are not NaN or Inf at idx
+    assert np.isnan(app_mag_r[idx]).any() == False, f"app_mag_r has NaN values at {np.where(np.isnan(app_mag_r))}"
+    assert np.isnan(z_eff[idx]).any() == False, f"z_eff has NaN values at {np.where(np.isnan(z_eff))}"
+    assert z_eff[idx].all() > 0.001, f"z_eff has too low values at {np.where(z_eff <= 0.001)}"
     np.put(abs_mag_R, idx, app_mag_to_abs_mag(app_mag_r[idx], z_eff[idx]))
     np.put(abs_mag_R_k, idx, k_correct(abs_mag_R[idx], z_eff[idx], g_r_apparent[idx], band='r'))
     np.put(abs_mag_G, idx, app_mag_to_abs_mag(app_mag_g[idx], z_eff[idx]))
@@ -1550,6 +1552,8 @@ def update_properties_for_indices(idx, app_mag_r, app_mag_g, g_r_apparent, z_eff
     G_R_k = abs_mag_G_k - abs_mag_R_k
     np.put(gmr_best, idx, G_R_k[idx])
     lookup = dn4000lookup()
+    assert np.isnan(abs_mag_R_k[idx]).any() == False, f"abs_mag_R_k[idx] has NaN values at {np.where(np.isnan(abs_mag_R_k[idx]))}"
+    assert np.isnan(G_R_k[idx]).any() == False, f"G_R_k[idx] has NaN values at {np.where(np.isnan(G_R_k[idx]))}"
     np.put(dn4000_model, idx, lookup.query(abs_mag_R_k[idx], G_R_k[idx]))
     np.put(quiescent, idx, is_quiescent_BGS_dn4000(log_L_gal[idx], dn4000_model[idx], G_R_k[idx]))
 

py/pyutils.py
@@ -64,6 +64,9 @@ ABS_MAG_MIDPOINTS = np.append(np.append([-23], 0.5*(ABS_MAG_BINS[1:-1] + ABS_MAG
 
 QUIESCENT_BINS = np.array([0.0, 1.0]) 
 
+L_gal_bins = np.logspace(6, 12.5, 40)
+L_gal_labels = L_gal_bins[0:len(L_gal_bins)-1]
+
 ################################
 
 DEGREES_ON_SPHERE = 41253
@@ -711,7 +714,7 @@ class dn4000lookup:
             raise ValueError(f"File {file} does not exist. The Dn4000 lookup table must be built first; see BGS_study.ipynb.")
         self.tree, self.dn4000_lookup = pickle.load(open(file, 'rb'))
 
-    def query(self, abs_mag_array, gmr_array, k=100):
+    def query(self, abs_mag_array, gmr_array, k=50):
         query_points = np.vstack((abs_mag_array * self.METRIC_MAG, gmr_array * self.METRIC_GMR)).T  # Scale the query points
         distances, indices = self.tree.query(query_points, k=k)  # Query the KDTree for multiple points
         print(np.shape(distances), np.shape(indices))
@@ -1501,3 +1504,85 @@ def is_quiescent_BGS_gmr(logLgal, G_R_k):
 
     #return G_R_k > GLOBAL_RED_COLOR_CUT
 
+
+
+
+###################################################################
+# MCMC Log Processing
+###################################################################
+
+def fsat_variance_from_saved():
+    """
+    Load the fsat variance from the saved file.
+    """
+    if os.path.exists(FSAT_VALUES_FROM_LOGS):
+        fsat_arr, fsatr_arr, fsatb_arr = np.load(FSAT_VALUES_FROM_LOGS)
+        fsat_std = np.std(fsat_arr, axis=0)
+        fsatr_std = np.std(fsatr_arr, axis=0)
+        fsatb_std = np.std(fsatb_arr, axis=0)
+        fsat_mean = np.mean(fsat_arr, axis=0)
+        fsatr_mean = np.mean(fsatr_arr, axis=0)
+        fsatb_mean = np.mean(fsatb_arr, axis=0)
+        return fsat_std, fsatr_std, fsatb_std, fsat_mean, fsatr_mean, fsatb_mean
+    else:
+        print(f"WARNING: {FSAT_VALUES_FROM_LOGS} does not exist. Cannot load variance. Call extract_variance_from_log(path) to create it.")
+        return None
+
+def extract_variance_from_log(logpath: str):
+    """
+    Extracts the fsat variance from the MCMC log file.
+    """
+    FSAT_BINCOUNT = len(L_gal_bins) 
+    fsat_len = 0
+
+    with open(logpath) as fp:
+        # First get total number
+        for line in fp:
+            if line.startswith("fsat> 0 "):
+                fsat_len += 1
+
+        fsat_arr = np.zeros((fsat_len, FSAT_BINCOUNT))
+        fsatr_arr = np.zeros((fsat_len, FSAT_BINCOUNT))
+        fsatb_arr = np.zeros((fsat_len, FSAT_BINCOUNT))
+
+        # Then go back to the beginning and read the values
+        fp.seek(0)
+        current_row = 0  # Track the current row in the arrays
+        for line in fp:
+            if line.startswith("fsat> "):
+                values = line.split(' ')
+                if values[1].isdigit():
+                    bin_i = int(values[1].strip())
+                    fsat = float(values[2].strip())
+                    fsatr = float(values[3].strip())
+                    fsatb = float(values[4].strip())
+                    fsat_arr[current_row, bin_i] = fsat
+                    fsatr_arr[current_row, bin_i] = fsatr
+                    fsatb_arr[current_row, bin_i] = fsatb
+
+                    # Increment the row counter when the last bin is processed
+                    if bin_i == FSAT_BINCOUNT - 1:
+                        current_row += 1
+
+            if current_row >= fsat_len:
+                break
+
+    # Save off the arrays of values
+    if os.path.exists(FSAT_VALUES_FROM_LOGS):
+        print(f"WARNING: {FSAT_VALUES_FROM_LOGS} already exists. Will not overwrite.")
+    else:
+        np.save(FSAT_VALUES_FROM_LOGS, (fsat_arr, fsatr_arr, fsatb_arr))
+
+    # Calculate the std, mean
+    fsat_std = np.std(fsat_arr, axis=0)
+    fsatr_std = np.std(fsatr_arr, axis=0)
+    fsatb_std = np.std(fsatb_arr, axis=0)
+    fsat_mean = np.mean(fsat_arr, axis=0)
+    fsatr_mean = np.mean(fsatr_arr, axis=0)
+    fsatb_mean = np.mean(fsatb_arr, axis=0)
+
+    # TODO LHMR 
+
+    # TODO Lsat?
+
+    return fsat_std, fsatr_std, fsatb_std, fsat_mean, fsatr_mean, fsatb_mean